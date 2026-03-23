f9f424827d6c8429bb2fca763c1664f65c88cf5e
ignacioserna <86605364+ignacioserna@users.noreply.github.com>
2026-03-23 10:30:20 +0000

    Deploying to gh-pages from @ ignacioserna/ignacioserna.github.io@9f4e5796498b0e31b6e523fff9934c167e9c187a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,6 +459,11 @@ ninja.data = [{
           description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
+            },},{id: "teachings-machine-behavior-and-machine-culture",
+          title: 'Machine Behavior and Machine Culture',
+          description: "",
+          section: "Teachings",handler: () => {
+              window.location.href = "/teachings/machine_behavior/";
             },},{id: "teachings-ai-machine-learning-foundations",
           title: 'AI: Machine Learning Foundations',
           description: "",
